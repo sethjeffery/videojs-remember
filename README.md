a2c15071e0d1a4faabc841daf9297bd9736ce9de
Seth Jeffery <seth@quipper.com>
2015-08-17 16:37:51 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# videojs-rememberplace
-Remembers the last place the user was at when watching a video, using localStorage.
+# Video.js Remember
+
+A plugin for video.js to remember the last place you were at when watching a video.
+
+## Getting Started
+
+Once you've added the plugin script to your page, you can use it with any video:
+
+```html
+<script src="video.js"></script>
+<script src="videojs-remember.js"></script>
+<script>
+  videojs(document.querySelector('video')).remember({"localStorageKey": "videojs.remember.myvideo"});
+</script>
+```
+
+## Documentation
+### Plugin Options
+
+#### localStorageKey
+Desc: The key name to read and write the current time of the current video in local storage.
+Type: `string`
+
+#### sessionStorageKey
+Desc: The key name to read and write the current time of the current video in session storage.
+Type: `string`
+
+## Release History
+
+ - 0.1.0: Initial release
---
+++ src/videojs-remember.js
@@ -0,0 +1,48 @@
+(function(window, videojs) {
+  'use strict';
+
+  var plugin = function(options) {
+    var player = this;
+    var prefix = localStorage['videojs.remember.prefix'] || '';
+
+    this.on('timeupdate', time_updated);
+    this.on('ended', time_updated);
+
+    function time_updated(time_update_event){
+      var current_time = this.currentTime();
+      var duration = this.duration();
+      var time = Math.floor(current_time);
+
+      if(time > duration || time_update_event.type === "ended") {
+        time = 0;
+      }
+
+      if (options.localStorageKey) {
+        localStorage[prefix + options.localStorageKey] = time;
+      }
+
+      if (options.sessionStorageKey) {
+        sessionStorage[prefix + options.sessionStorageKey] = time;
+      }
+    }
+
+    player.ready(function() {
+      if (!player.initialSeek) {
+        if (options.localStorageKey) {
+          player.initialSeek = parseInt(localStorage[prefix + options.localStorageKey]);
+        }
+
+        if (options.sessionStorageKey) {
+          player.initialSeek = parseInt(sessionStorage[prefix + options.sessionStorageKey]);
+        }
+
+        player.one('playing', function() {
+          player.currentTime(player.initialSeek);
+        });
+      }
+    });
+  };
+
+  // register the plugin
+  videojs.plugin('remember', plugin);
+})(window, window.videojs);
